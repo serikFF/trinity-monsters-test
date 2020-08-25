bae5e81e996750fb7a58ce9c2d09c8eeb36cb934
Sergey Roslyakov <s.rosliakov@redmadrobot.com>
2020-08-25 11:00:25 +0300

    Create project

---
+++ News/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  News
+//
+//  Created by Sergei Rosliakov on 25.08.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
